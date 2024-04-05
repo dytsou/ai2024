50fe129e24e28ad4747fd8ac8d28a1520d418a69
dytsou <d.y.tsou@gmail.com>
2024-04-05 16:02:24 +0800

    task 6 finished

--- HW2/HW2/astar_time.py
+++ HW2/HW2/astar_time.py
@@ -1,11 +1,73 @@
 import csv
+import heapq
 edgeFile = 'edges.csv'
 heuristicFile = 'heuristic.csv'
+
+def time_converter(distance, speed):
+    return distance / speed * 3.6 # Convert distance to time in seconds
 
 
 def astar_time(start, end):
     # Begin your code (Part 6)
-    raise NotImplementedError("To be implemented")
+    with open(edgeFile, 'r') as file:
+        header = next(file).strip().split(',')  # Skip the header
+    graph = {}
+    with open(edgeFile, 'r') as file:
+        next(file)
+        reader = csv.reader(file)
+        for row in reader:
+            s = int(row[0])
+            t = int(row[1])
+            d = float(row[2])
+            l = float(row[3])
+            if s not in graph:
+                graph[s] = []
+            graph[s].append((t, time_converter(d, l)))
+            if t not in graph:
+                graph[t] = []
+            graph[t].append((s, time_converter(d, l)))
+    
+    with open(heuristicFile, 'r') as file:
+        header = next(file).strip().split(',')
+    heuristic = {}
+    with open(heuristicFile, 'r') as file:
+        next(file)
+        reader = csv.reader(file)
+        for row in reader:
+            node = int(row[0])
+            h = float(row[1])
+            heuristic[node] = h
+    
+    visited = {}
+    time = {}
+    parent = {}
+    for node in graph:
+        visited[node] = False
+        time[node] = float('inf')
+        parent[node] = None
+    time[start] = 0
+    visited_times = 0
+    pq = []
+    heapq.heappush(pq, (heuristic[start], start))
+    while pq is not None:
+        visited_times += 1
+        current = heapq.heappop(pq)[1]
+        if current == end:
+            break
+        if visited[current]:
+            continue
+        visited[current] = True
+        for neighbor, weight in graph[current]:
+            if time[current] + weight < time[neighbor]:
+                time[neighbor] = time[current] + weight
+                parent[neighbor] = current
+                heapq.heappush(pq, (time[neighbor] + heuristic[neighbor], neighbor)) 
+    curr_path = []
+    current = end
+    while current is not None:
+        curr_path.insert(0, current)
+        current = parent[current]
+    return curr_path, time[end], visited_times   
     # End your code (Part 6)
 
 
